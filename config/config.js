047ba1754415b030b380af863f1d0679649c6515
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-01-26 12:33:41 -0700

    changed config.json to config.js, added env variables

--- config/config.js
+++ config/config.js
@@ -1,9 +1,11 @@
-{
+require('dotenv').config();
+
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "exampledb",
-    "host": "localhost",
+    "username": process.env.USER,
+    "password": process.env.PASSWORD,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST,
     "dialect": "mysql"
   },
   "test": {

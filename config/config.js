df047d0efa4f8e4f88244b62d6e3c6824c40acce
ghost0fharambe <43300063+ghost0fharambe@users.noreply.github.com>
2019-01-26 12:37:06 -0700

    updated linting

--- config/config.js
+++ config/config.js
@@ -1,23 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  "development": {
-    "username": process.env.USER,
-    "password": process.env.PASSWORD,
-    "database": process.env.DATABASE,
-    "host": process.env.HOST,
-    "dialect": "mysql"
+  development: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: "mysql"
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "testdb",
-    "host": "localhost",
-    "dialect": "mysql",
-    "logging": false
+  test: {
+    username: "root",
+    password: null,
+    database: "testdb",
+    host: "localhost",
+    dialect: "mysql",
+    logging: false
   },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
+  production: {
+    // eslint-disable-next-line camelcase
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
   }
-}
+};

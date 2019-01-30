87418e30984d3b334028ce741cff5df57ad67000
james barker <jamesbarker585@gmail.com>
2019-01-29 21:28:33 -0700

    new changes to html

---
+++ config/config.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.USER,
+    password: process.env.PASS,
+    database: "traffic_db",
+    host: process.env.HOST,
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    // eslint-disable-next-line camelcase
+    use_env_variable: "JAWSDB_URL",
+    dialect: "mysql"
+  }
+};

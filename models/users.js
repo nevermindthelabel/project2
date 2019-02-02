c1547caaa466db0319a55ab80d50350e5b605178
nevermindthelabel <nevermindthelabel@gmail.com>
2019-02-02 15:33:36 -0700

    updated users model to contain username and password

--- models/users.js
+++ models/users.js
@@ -1,7 +1,30 @@
+var bcrypt = require("bcrypt-nodejs");
+
 module.exports = function(sequelize, DataTypes) {
   var Users = sequelize.define("Users", {
     // Giving the Username model a name of type STRING
-    userName: DataTypes.STRING
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+  Users.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+  // Hooks are automatic methods that run during various phases of the User Model lifecycle
+  // In this case, before a NewUser is created, we will automatically hash their password
+  Users.hook("beforeCreate", function(users) {
+    users.password = bcrypt.hashSync(
+      users.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
   });
 
   Users.associate = function(models) {
@@ -9,6 +32,6 @@ module.exports = function(sequelize, DataTypes) {
     // When an Author is deleted, also delete any associated Posts
     Users.hasMany(models.Reports);
   };
-
   return Users;
 };
+

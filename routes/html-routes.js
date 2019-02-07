f02cf58c23ac1c94afad0d0b75239e761b51c7a9
nevermindthelabel <nevermindthelabel@gmail.com>
2019-02-07 07:42:54 -0700

    fixed TrafficMon in msg to user

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
   //GET Route to root page
   app.get("/", function(req, res) {
     res.render("index", {
-      msg: "Traffic Mon!"
+      msg: "TrafficMon!"
     });
   });
   //GET Route to display submit new user form
